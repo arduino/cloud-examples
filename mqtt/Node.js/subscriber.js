3e02464405e99260165ab42456c6b198f339376a
Sandeep Mistry <s.mistry@arduino.cc>
2015-09-30 16:58:39 -0400

    Initial Node.js client example

---
+++ mqtt/Node.js/subscriber.js
@@ -0,0 +1,38 @@
+var mqtt = require('mqtt'); // https://github.com/mqttjs/MQTT.js
+
+var brokerURI = 'mqtts://mqtt.arduino.cc';
+
+// use the values shown after logging into https://cloud.arduino.cc
+var username  = 'username';
+var password  = 'password';
+var baseTopic = '/' + username;
+var topic     = baseTopic + '/001';
+
+// default message
+var message   = '0';
+
+if (process.argv.length > 2) {
+  // use command line argument as message
+  message = process.argv[2]
+}
+
+// create client and connect
+var client  = mqtt.connect(brokerURI, {
+  username: username,
+  password: password
+});
+
+// attatch event listener for 'connect' event
+client.on('connect', function () {
+  // client has connected to the broker
+  console.log('connected');
+
+  // subscribe to the topic
+  client.subscribe(topic);
+});
+
+// attatch event listener for 'message' event
+client.on('message', function (topic, message) {
+  // print out topic and message
+  console.log('Received message: ' + topic + ' ' + message);
+});
